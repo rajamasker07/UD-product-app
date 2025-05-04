b17bc2171e528b8e4b296986077895bad6ec3d39
rosyidohyeah <rosyidohyeah@gmail.com>
2025-05-04 11:13:29 +0000

    add log

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+dotenv.config();
+
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(process.env.DATABASE_URL)],
   controllers: [AppController],
   providers: [AppService],
 })
