9f7061be9ba48c6e182f2cef4bb8ec827f3dde81
rosyidohyeah <rosyidohyeah@gmail.com>
2025-05-06 22:30:15 +0000

    change url product

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ResponseInterceptor } from './response/response.interceptor';
-import { ResponseInterface } from './response/response.interface';
 
-@Controller()
+@Controller('product')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  // @UseInterceptors(new ResponseInterceptor())
   getHello() {
     return {
       status: 'success',
