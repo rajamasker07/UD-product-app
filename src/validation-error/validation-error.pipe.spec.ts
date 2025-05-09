388d7e1816e35fce9a3fccd8de8205ec4639fab2
rosyidohyeah <rosyidohyeah@gmail.com>
2025-05-09 19:49:08 +0000

    handler badRequest response

---
+++ src/validation-error/validation-error.pipe.spec.ts
@@ -0,0 +1,7 @@
+import { ValidationErrorPipe } from './validation-error.pipe';
+
+describe('ValidationErrorPipe', () => {
+  it('should be defined', () => {
+    expect(new ValidationErrorPipe()).toBeDefined();
+  });
+});
---
+++ src/validation-error/validation-error.pipe.ts
@@ -0,0 +1,36 @@
+import { ArgumentMetadata, BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { validate } from 'class-validator';
+
+@Injectable()
+export class ValidationErrorPipe implements PipeTransform {
+  async transform(value: any, { metatype }: ArgumentMetadata) {
+    if (!metatype || !this.toValidate(metatype)) {
+      return value;
+    }
+    const object = plainToInstance(metatype, value);
+    const errors = await validate(object);
+
+    if (errors.length > 0) {
+      const formattedErrors = errors.map(error => {
+        const constraints = Object.values(error.constraints);
+        return {
+          path: error.property,
+          message: constraints.join(', '),
+        };
+      });
+
+      throw new BadRequestException({
+        status: 'error',
+        message: 'validation error',
+        errors: formattedErrors,
+      });
+    }
+    return value;
+  }
+
+  private toValidate(metatype: Function): boolean {
+    const types: Function[] = [String, Boolean, Number, Array, Object];
+    return !types.includes(metatype);
+  }
+}
